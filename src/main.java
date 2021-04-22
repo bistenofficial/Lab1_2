f94c34d8bf26874f1566a2339f87744eb4804f3c
bistenofficial <maks.kirov.2002@mail.ru>
2021-04-22 14:29:18 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,12 @@
+import  java.util.Scanner;
+
+public class main
+{
+    public int priceRub;
+    public static void mn(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите фамилию");
+        price
+    }
+}
